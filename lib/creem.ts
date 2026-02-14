2e2618be870c9ad44226cdeaee1781326d70fe13
fishfl <fishfl@163.com>
2026-02-14 11:25:43 +0800

    testMode

--- lib/creem.ts
+++ lib/creem.ts
@@ -3,5 +3,6 @@ import { createCreem } from 'creem_io';
 // Initialize the Creem client
 export const creem = createCreem({
   apiKey: process.env.CREEM_API_KEY!,
-  testMode: process.env.NODE_ENV !== 'production', // Automatically switch based on environment
+  // testMode: process.env.NODE_ENV !== 'production', // Automatically switch based on environment
+  testMode: true
 });
